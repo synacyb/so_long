8ff19d4e128447400a97bb01e58aa9a37c17728d
Ayoub Adouay <ayadouay@c4r10p8.1337.ma>
2025-03-01 16:15:43 +0000

    adding

--- helper-functions/init_win.c
+++ helper-functions/init_win.c
@@ -6,16 +6,16 @@
 /*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 15:52:40 by ayadouay          #+#    #+#             */
-/*   Updated: 2025/03/01 15:57:03 by ayadouay         ###   ########.fr       */
+/*   Updated: 2025/03/01 16:11:26 by ayadouay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void    *init_win(t_textur  *data, int colums, int rows)
+void    *init_win(t_textur  *data, int length, int width)
 {
     data->mlx = mlx_init();
     if (!data->mlx)
         return NULL;
-    
+    data->window = mlx_new_window(data->mlx, length * 35, width * 35, "so_long");
 }
