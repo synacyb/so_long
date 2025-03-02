22ae03199f72e2395b4042e375835826cc602a84
Ayoub Adouay <ayadouay@c4r9p7.1337.ma>
2025-03-02 16:03:40 +0000

    adding

--- helper-functions/check_map.c
+++ helper-functions/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:32:03 by ayadouay          #+#    #+#             */
-/*   Updated: 2025/03/01 12:03:02 by ayadouay         ###   ########.fr       */
+/*   Updated: 2025/03/02 13:07:09 by ayadouay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,32 @@ void	creat_matrix(int fd, t_map *data)
 	data->copy_map[index] = NULL;
 	close(fd);
 }
-
+char *ft_strcpy(char *s1)
+{
+	int i = 0;
+	char *s2;
+	s2 = malloc(ft_strlen(s1) + 1);
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return s2;
+}
+char **ft_strcpymap(char **map, int lint, int colums)
+{
+	char **cpymap;
+	cpymap =  malloc(sizeof(char*) * (lint + 1));
+	int i = 0;
+	int j;
+	while (i < lint)
+	{
+		cpymap[i] = ft_strcpy(map[i]);
+		i++;
+	}
+	return (cpymap);
+}
 int	check_map(char *file_name, t_map *data)
 {
 	int fd = open(file_name, O_RDONLY);
@@ -55,6 +80,7 @@ int	check_map(char *file_name, t_map *data)
 	close(fd);
 	fd = open(file_name, O_RDONLY);
 	creat_matrix(fd, data);
+	data->map = ft_strcpymap(data->copy_map, data->rows, data->columns);
 	data->columns = ft_strlen(data->copy_map[0]) - 1;
 	if(data->columns == -1)
 		return (perror("map is empty!"), 0);
--- main.c
+++ main.c
@@ -6,12 +6,24 @@
 /*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:50:17 by ayadouay          #+#    #+#             */
-/*   Updated: 2025/03/01 15:58:37 by ayadouay         ###   ########.fr       */
+/*   Updated: 2025/03/02 15:15:22 by ayadouay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int    init_wind(t_textur  *data, int length, int width)
+{
+    data->mlx = mlx_init();
+    if (!data->mlx)
+        return (custom_error("MLX initialization failed", 0));
+    data->window = mlx_new_window(data->mlx, width * 40, length * 40, "so_long");
+    if (!data->window)
+        return (custom_error("Window creation failed", 0));
+	mlx_loop(data->mlx);
+    data->wall_img = mlx_xpm_file_to_image(data->mlx, "../wall_xpm/wall.xpm", NULL, NULL);
+    data->floor_img = mlx_xpm_file_to_image(data->mlx, "../floor/floor.xpm", NULL, NULL);
+}
 int main(int ac, char **av)
 {
 	t_map	map;
@@ -23,4 +35,8 @@ int main(int ac, char **av)
 		custom_error("map name is invalid!", 1);
 	if (check_map(av[1], &map) == 0)
 		return (free_matrix(&map), 1);
+	
+		if (!init_wind(&textur, map.rows, map.columns))
+			return(free_matrix(&map), 1);
+		
 }
