c68207ffac25beb988117e51c9ec98168a3d2852
Ayoub Adouay <ayadouay@c4r9p7.1337.ma>
2025-03-02 17:17:17 +0000

    adding

--- helper-functions/check_map_name.c
+++ helper-functions/check_map_name.c
@@ -6,7 +6,7 @@
 /*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:24:47 by ayadouay          #+#    #+#             */
-/*   Updated: 2025/02/27 08:10:22 by ayadouay         ###   ########.fr       */
+/*   Updated: 2025/03/02 17:16:14 by ayadouay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int check_map_name(char *str)
 {
     int len = ft_strlen(str);
-    if (len > 4 && str[len - 1] == 'r' && str[len - 2] == 'e' && str[len - 3] == 'b' && str[len - 4] == '.')
+    if (len > 4 && str[len - 1] == 'r' && str[len - 2] == 'e' && str[len - 3] == 'b' && str[len - 4] == '.' && str[len - 5] != '/')
         return (1);
     return (0);
 }
