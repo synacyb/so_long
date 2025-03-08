df693f6e9ad04b702baaa7a72ed874d1fdd1df98
Ayoub Adouay <ayadouay@c4r10p6.1337.ma>
2025-03-08 07:51:27 +0000

    adding

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ayadouay <ayadouay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:16:00 by ayadouay          #+#    #+#             */
-/*   Updated: 2025/02/28 10:37:54 by ayadouay         ###   ########.fr       */
+/*   Updated: 2025/03/08 07:51:09 by ayadouay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *str)
 	size_t	index;
 
 	index = 0;
-	if(str == NULL)
+	if (str == NULL)
 		return (0);
 	while (*str != '\0')
 	{
